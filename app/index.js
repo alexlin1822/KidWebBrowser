2586a5eec4713dcff24f5f30ae9dcffb84ee68ed
Alex <alexlin1822@hotmail.com>
2023-09-02 10:10:23 -0400

    Updated License

--- app/index.js
+++ app/index.js
@@ -1,3 +1,20 @@
+/**
+ *
+ * Copyright (C) 2023 Alex Lin <alexlin1822@hotmail.com>
+ *
+ * This program is free Open-source software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 import React, { useState } from "react";
 import { Switch, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
